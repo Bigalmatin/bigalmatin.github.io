b837127693b3cf1a05759c1107bfb0402ab029b6
Bigalmatin <bigalmatin@gmail.com>
2018-01-24 23:14:47 +0600

    init commit

---
+++ index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<meta charset="utf-8" http-equiv="content-type">
+	<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<title>SenI'm hostel</title>
+
+</head>
+<body>
+	<div class="wrapper">
+		
+		<div class="box1">
+			<div class="block1">
+				<div class="head">
+					<!-- img class="cornerimg" src="img/corner.png"> -->
+					<img class="logoimg" src="img/SenI’m1.png">
+
+                 <div class="callBox">
+                    <span class="phone">+7 (747) 165-36-69</span>
+                    <span class="adress"><a href="#block90" class="tele">г.Алматы, ул.Валиханова 53</a><br></span>
+                   </div>
+                   <div class="lang">
+                   	
+                   </div>
+  				</div>
+				
+				<div class="head2">
+					<h1 class="slogan1">Алматы ортасында  орналасқан  SenI’m  хостелі</h1>
+        			<h2 class="slogan2">Еуропалық  хостел  күніне  2 490 тг-ден</h2>
+        			
+        			<div class="connect">
+        				<div class="video">	
+        				 <video width="600" height="350" controls="controls" autoplay="autoplay" poster="video/duel.jpg">
+   <source src="video/video.ogv" type='video/ogg; codecs="theora, vorbis"'>
+   <source src="video/video.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+   <source src="video/video.webm" type='video/webm; codecs="vp8, vorbis"'>
+   Тег video не поддерживается вашим браузером. 
+   <a href="video/video.mp4">Скачайте видео</a>.</div>
+        					
+        					
+        				
+	<form method="post" action="numbers/sobshenie.php"> <div class="form modal" id="myMMM">
+            				
+     <h3>Нөмеріңді  қалдыру  арқылы, 
+     Алматының ЕҢ КЕРЕМЕТ демалыс орындарының тізімін ал
+	</h3>
+	<input type="name" class="name" name="name" placeholder="Есім">
+	<input type="phone" name="message" class="phone" placeholder="Телефон"><br>
+	<button type="submit" id="myAAA">Номер қалдыру</button>
+            				
+    </div></form>	
+        			</div>
+				</div>
+
+				<div class="line"></div>
+			</div>
+		</div>
+		
+		<div class="box2">
+			<div class="block2">
+			<h1 class="slogan3">Біз   әрбір  клиент  үшін  <span>«RELAX zone»</span>  қамтамасыз етеміз</h1>
+			<div class="relax">
+			
+			<div class="spiski1">                            				<div class="spisok1"><img src="img/003-team.png">Ай сайын <strong>70</strong> клиент <br><strong>18</strong>  қаладан</div>
+				<div class="spisok2"><img src="img/004-broom.png">Евроқұрылыс, <br>күнделікті  тазалық&nbsp;</div>
+				<div class="spisok3"><img src="img/001-washing-machine.png" alt="">Толықтай техникалық жағдай<br>(фен, үтік, ас жылытқаш, кір машина)&nbsp;</div>
+			</div>
+			
+				<div class="spiski2">
+				<div class="spisok4"><img src="img/006-24-hours.png"><strong>24</strong>  сағаттық  еркіндік&nbsp;</div>
+				<div class="spisok5"><img src="img/005-wifi.png">Тегін <strong>Wi-Fi, PS 4</strong>, кітапхана&nbsp;</div>
+				<div class="spisok6"><img src="img/002-bed.png">Уақытылы төсек  ауыстыру&nbsp;</div>
+				</div>
+			</div>
+			<div class="line"></div>
+			</div>
+		</div>
+	
+	<div class="box3">
+		<div class="block3">
+			<h1>Хостеліміздің  түрлері мен  бағалары</h1>
+				<div class="pred">
+				<div class="standard">
+				<h3>Стандарт </h3>
+				<img src="img/img4.jpg">
+				<p>1 500 тг-ден адам басына
+				   6- 10 адамдық бөлмеде 
+				   орналасады. Бөлмеде  жайлы
+				   орындар мен шкафтар, айна,
+				   тегін Wi-Fi бар)
+				</p>
+				<button id="myAAA"><a href="#openModal">Номер қалдыру</a></button>
+				</div>
+				<div class="vip">
+				<h3>VIP</h3>
+				<img src="img/img5.jpg">
+				<p>2 500 тг-ден  адам  басына
+				   2-4  адамдық  бөлмеде орналасады 
+				   Арнайы демалыс бөлмесі мен ойындар,
+				   жеке кітапхана, тегін шай мен печенье
+				   Wi-Fi  қарастырылған.
+				</p>
+				<button id="myAAA"><a href="#openModal">Номер қалдыру</a></button>
+				</div>
+				</div>
+				<div class="line"></div>
+		</div>
+	</div>
+		
+		<div class="box4">
+		<div class="block4">
+			<h1>Хостел  жанында  нендей  ТАНЫМАЛ  орындар  бар?</h1>
+			<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 8</div>
+    <img src="img/jibek.jpg" style="width:100%">
+    <div class="text">7 минуттық  жерде  «Жібек  жолы»  метросы</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 8</div>
+    <img src="img/kaz.jpg" style="width:100%">
+    <div class="text">5 жұлдызды «Қазақстан  қонақ  үйі»</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 8</div>
+    <img class="medeu" src="img/medeu.jpg" style="width:100%">
+    <div class="text">15 минуттық  жерде «Медеу  мұз   айдыны»</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">4 / 8</div>
+    <img class="medeu" src="img/abai.jpg" style="width:100%">
+    <div class="text">Абай  атындағы  опера және   балет  театры</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">5 / 8</div>
+    <img class="medeu" src="img/nocafe.jpg" style="width:100%">
+    <div class="text">2  антикафе</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">6 / 8</div>
+    <img class="medeu" src="img/sulu.jpg" style="width:100%">
+    <div class="text">7  сүлулық  салоны</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">7 / 8</div>
+    <img class="medeu" src="img/arbat.jpg" style="width:100%">
+    <div class="text">Алматының  ордасы – Арбат  әлемдік  мерекелік және жәрмеңкелер  мекені</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">8 / 8</div>
+    <img class="medeu" src="img/koktobe.jpg" style="width:100%">
+    <div class="text">Көктөбе шыңына жақын орналасқан</div>
+  </div>
+
+
+
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span>
+  <span class="dot" onclick="currentSlide(6)"></span>
+  <span class="dot" onclick="currentSlide(7)"></span>
+  <span class="dot" onclick="currentSlide(7)"></span>
+</div>
+<div class="line"></div>
+		</div>
+		</div>
+	
+	<div class="box5">
+		<div class="block5">
+			<h1>Алматы  жастарына «January»  лотореялық  жәрмеңке  ұсынамыз</h1>
+			<div class="kuh"><form> <div class="form">
+            				
+            					<h3>Егер  сіз  дәл  қазір   нөмер  қалдырсаңы  біз  сізге  лотореялық  билет нөмерін жібереміз</h3>
+								<input type="name" class="name" placeholder="Есім">
+								<input type="phone" class="phone" placeholder="Телефон"><br>
+								<button id="myAAA">Номер қалдыру</button>
+								
+            				
+           					 </div></form>
+           					 <div class="callback"><img src="img/cally.png" alt=""><p class="instit"><a href="https://www.instagram.com/hostelalmaty_senim/">«senI’mhostel»</a>  инстаграм әлеуметтік  желісінде тікелей  эфирде   кездейсоқ  таңдалған  нөмерлерге біз:
+2  жеңімпазға  4 күндік  тегін  хостелге  орналасу  ұсынамыз
+</p></div>
+           					 <!-- <img class="insta1" src="img/insta.jpg"> -->
+           					</div>
+		</div>
+	
+	</div>
+	<div class="box6">
+		<div class="block6">
+			<script src="http://megatimer.ru/s/407b108c401bb2deb0b579de42b23ae4.js"></script>
+		</div>
+		<div class="line"></div>
+	</div>
+	
+	<div class="box7">
+		<div class="block7">
+			<h1> Сізге  жат  хостел  қызықтары</h1>
+		<!-- The video -->
+<video autoplay muted loop id="myVideo">
+  <source src="video/video1.mp4" type="video/mp4">
+</video>
+
+<!-- Optional: some overlay text to describe the video -->
+<div class="content">
+  <h1 class="video1h1">ALMATY CITY</h1>
+ 
+  <!-- Use a button to pause/play the video with JavaScript -->
+  <button id="myBtn" onclick="myFunction()">Pause</button>
+</div>
+		</div>
+	</div>
+
+	<div class="box221">
+		
+		<div class="block222">
+			<div class="gulsezim">
+				<h2><span>“senI’m”</span> компаниясының  құраушысы</h2>
+				<img src="img/manager2.jpg">
+				<h3>Гүлімжан Нұрлыханқызы</h3>
+				<p class="gray1">«Ең алғаш осы идея басымызға келгенде, қалтамызда ақша да болмаған еді. Тек ашу керек, елімізге қажет деген ұстанымда алға баса бердік. Елімізде Еуропалық үлгіде хостелдер болғанын қалаушы едім...»</p>
+				<button><a href="#openModal">Тікелей байланысқым келеді</a></button>
+			</div>
+			<div class="dauletbek">
+				<h2><span>“senI’m”</span> компаниясының  құраушысы</h2>
+				<img src="img/manager1.jpg">
+				<h3>Рысдаулет  Тәжібайұлы</h3>
+				<p class="gray2">«Мен өзімнің ашқан хостелдеріме үйімдей қараймын және әрқашан дамуын жіті баұылаймын. Себебі, елімізде көптеген жастар қолжетімсіз пәтер жалдап жүр. Қазіргі заман мобильді заман болғадықтан – елімізге келуші қонақтар үшін де хостел маңызды орын алады...»</p>
+				<button><a href="#openModal">Тікелей байланысқым келеді</a></button>
+			</div>
+		</div>
+	</div>
+	
+<div class="steshi">
+	<div id="openModal" class="modalDialog">
+
+    <div class="form1 centring">
+
+        <a href="#close" title="Закрыть" class="close">X</a>
+
+        <h3>Нөмеріңді  қалдыру  арқылы, 
+                                               Алматының ЕҢ КЕРЕМЕТ демалыс орындарының тізімін ал
+								</h3>
+        <input type="name" class="name1" placeholder="Есім">
+								<input type="phone" class="phone1" placeholder="Телефон"><br>
+								<button id="myAAA"><a href="#openModal">Номер қалдыру</a></button>
+    </div>
+
+</div>
+</div>
+	<div class="box9">
+		<div class="block9" id="block90">
+<div class="block9child">
+<a class="dg-widget-link" href="http://2gis.kz/almaty/firm/70000001030663446/center/76.951915,43.253248/zoom/16?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть на карте Алматы</a><div class="dg-widget-link"><a href="http://2gis.kz/almaty/center/76.951915,43.253248/zoom/16/routeTab/rsType/bus/to/76.951915,43.253248╎SenI`M, хостел?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти проезд до SenI`M, хостел</a></div><script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script><script charset="utf-8">new DGWidgetLoader({"width":1600,"height":450,"borderColor":"#a3a3a3","pos":{"lat":43.253248,"lon":76.951915,"zoom":16},"opt":{"city":"almaty"},"org":[{"id":"70000001030663446"}]});</script><noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в настройках вашего браузера.</noscript></div>
+
+		</div>
+	</div>
+		
+		<div class="box10">
+		<div class="block10">
+			<button id="myAAA" class="footcall"><a href="#openModal">Номер қалдыру</a></button>
+			<p>Нөмеріңді  қалдыру арқылы өмір салтыңды ауыстыр</p>
+			<div class="iconss">
+			<a href="https://vk.com/public160733370"><img src="img/slider/con2.png"></a>
+			<a href="https://www.instagram.com/hostelalmaty_senim/"><img src="img/slider/con1.png"></a>
+			<a href="https://www.facebook.com/pg/%D0%A5%D0%BE%D1%81%D1%82%D0%B5%D0%BB%D1%8C-Senim-%D0%90%D0%BB%D0%BC%D0%B0%D1%82%D1%8B-135707927047118/about/?entry_point=page_edit_dialog&tab=page_info"><img src="img/slider/facebook.png"></a>
+			</div>
+		</div>
+	</div>
+	</div>
+	<script src="script.js"></script>
+</body>
+</html>
